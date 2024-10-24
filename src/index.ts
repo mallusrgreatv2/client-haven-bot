902cef4d2eee6a2577f53b4f57645da128a59f2d
mallusrgreat <mallusrgreat@duck.com>
2024-10-24 22:11:54 +0530

    feat: save crash errors to file

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import './lib/setup.js';
 
 import { LogLevel, SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
-
+import { env } from 'process';
+import fs from 'node:fs';
+const intents = [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent];
+if (env.TLAUNCHER_CHECK_ENABLED === 'true') intents.push(GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences);
 const client = new SapphireClient({
 	defaultPrefix: '-',
 	caseInsensitiveCommands: true,
 	logger: {
 		level: LogLevel.Debug
 	},
-	intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
+	intents,
 	loadMessageCommandListeners: true,
 	allowedMentions: {
 		roles: [],
@@ -31,3 +34,21 @@ const main = async () => {
 };
 
 void main();
+export const isDev = env.NODE_ENV === 'development';
+
+const errorCode = (err: any) => {
+	try {
+		const files = fs
+			.readdirSync('/')
+			.filter((v) => /error.[0-9]+\.txt/.test(v))
+			.sort((a, b) => a.localeCompare(b))
+			.at(-1);
+		const lastNum = files ? +!files.match(/error.([0-9]+)\.txt/)?.[1] : 0;
+		fs.writeFileSync(`error.${lastNum + 1}.txt`, `${err}${err instanceof Error ? `\n\n${err.stack}` : ''}`);
+	} catch (fsError) {
+		console.error('Failed to write error log:', fsError);
+	}
+	process.exit(1);
+};
+process.on('uncaughtException', errorCode);
+process.on('unhandledRejection', errorCode);
